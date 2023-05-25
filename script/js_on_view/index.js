cc16679e7177a062e98c9904f6015840840dbe57
aonitcmtc <patchara_aon_1@hotmail.com>
2023-05-25 23:29:04 +0700

    add files sign up

---
+++ script/js_on_view/index.js
@@ -0,0 +1,13 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+
+script.onload = function() {
+  // jQuery has been loaded
+  // You can now use jQuery in your code
+  // Example:
+  $(document).ready(function() {
+    // jQuery code goes here
+
+  });
+};
+document.head.appendChild(script);
